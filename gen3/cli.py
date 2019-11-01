cfbc822504073ab17c69de3c5158531527a1eac6
Fantix King <fantix@uchicago.edu>
2019-10-31 23:49:34 -0500

    friendly help if CLI extras not installed

--- gen3/cli.py
+++ gen3/cli.py
@@ -7,5 +7,18 @@ def gen3():
     """Welcome to Gen3 Data Commons."""
 
 
-for ep in pkg_resources.iter_entry_points("gen3.cli"):
-    ep.load()
+def load_extras():
+    extras = {}
+    for i, ep in enumerate(pkg_resources.iter_entry_points("gen3.cli")):
+        try:
+            ep.load()
+        except pkg_resources.DistributionNotFound:
+            for extra in ep.extras:
+                extras.setdefault(extra, set()).add(ep.module_name)
+    if extras:
+        gen3.help += "\n\nExtra CLI modules not installed:\n\n\b\n"
+        for extra, modules in extras.items():
+            gen3.help += f"    gen3[{extra}]: {', '.join(modules)}\n"
+
+
+load_extras()
